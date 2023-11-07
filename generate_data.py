f761cd5e3408a08d5cbcb9b4ca95f6fd92526f17
Naman Kumar <namank2002@gmail.com>
2023-11-07 15:25:01 +0530

    faster plotting without using system command

--- generate_data.py
+++ generate_data.py
@@ -4,7 +4,7 @@ generate_data(
     data_dir="data",
     image_size=512,
     num_plates=500,
-    start_plate=12,
+    start_plate=None, # None for starting from scratch, or a number to continue from
     conditions_per_plate=4,
     mesh_size=1e-2,
     save_displacement=True,
