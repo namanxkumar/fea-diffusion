4501607bad9b7fa339cd7ede64bc684c24d61cfe
Naman Kumar <namank2002@gmail.com>
2023-11-13 00:04:03 +0530

    added args

--- train.py
+++ train.py
@@ -7,9 +7,11 @@ parser = argparse.ArgumentParser(description='Train model.')
 
 parser.add_argument('--data_dir', type=str, default='data', help='Data directory.')
 parser.add_argument('--sample_data_dir', type=str, default='sample_data', help='Sample data directory.')
+parser.add_argument('--num_sample_conditions_per_plate', type=int, default=1, help='Number of sample conditions per plate.')
 parser.add_argument('--results_dir', type=str, default='results', help='Results directory.')
 parser.add_argument('--image_size', type=int, default=256, help='Image size.')
 parser.add_argument('--batch_size', type=int, default=16, help='Batch size.')
+parser.add_argument('--num_gradient_accumulation_steps', type=int, default=1, help='Number of gradient accumulation steps.')
 parser.add_argument('--num_steps', type=int, default=100, help='Number of steps.')
 parser.add_argument('--num_steps_per_milestone', type=int, default=1000, help='Number of steps per milestone.')
 parser.add_argument('--ema_steps_per_milestone', type=int, default=10, help='EMA steps per milestone.')
@@ -28,8 +30,8 @@ trainer = Trainer(
     dataset_folder=args.data_dir,
     use_dataset_augmentation=True,
     sample_dataset_folder=args.sample_data_dir,
-    num_sample_conditions_per_plate=1,
-    num_gradient_accumulation_steps=1,
+    num_sample_conditions_per_plate=args.num_sample_conditions_per_plate,
+    num_gradient_accumulation_steps=args.num_gradient_accumulation_steps,
     dataset_image_size=args.image_size,
     train_batch_size=args.batch_size,
     train_learning_rate=args.learning_rate,
