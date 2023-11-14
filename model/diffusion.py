81252e7a6a3b572c6ca83db0c712106b11043948
Naman Kumar <namank2002@gmail.com>
2023-11-14 23:25:43 +0530

    bug fix

--- model/diffusion.py
+++ model/diffusion.py
@@ -270,7 +270,7 @@ class Trainer():
         accelerator = self.accelerator
         device = accelerator.device
 
-        checkpoint = torch.load(str(self.results_folder / f'model-{milestone}.pt'), map_location=device)
+        checkpoint = torch.load(str(self.results_folder / f'model-{milestone}'), map_location=device)
 
         model: nn.Module = self.accelerator.unwrap_model(self.model)
         model.load_state_dict(checkpoint['model'])
